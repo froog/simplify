07c489651a6e08a8860b701926e6b28193d8ae01
Daniel Pickering <froogalicious@gmail.com>
2013-07-30 01:09:30 +0100

    Deleted deprecated Simplify css and js files

--- javascript/simplify_draggable_on.js
+++
@@ -1,8 +0,0 @@
-//append custom loader (appendLoader is in jsparty/loader.js)
-appendLoader(function() {
-		//this is bad - fix it!
-		if (jQuery('.CMSMain #TreeTools #sortitems').attr('checked') == false) {
-			jQuery('.CMSMain #TreeTools #sortitems').click();
-		}
-});
-
--- javascript/simplify_hide_draggable.js
+++
@@ -1,6 +0,0 @@
-//append custom loader (appendLoader is in jsparty/loader.js)
-appendLoader(function() {
-	//Hide the drag and drop checkbox and its label
-	jQuery('.CMSMain #TreeTools #sortitems').hide().next('label').hide();
-});
-
